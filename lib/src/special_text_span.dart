480492190d1117922baf943e32ea7638ed2aa0f2
zmtzawqlp <zmtzawqlp@live.com>
2019-04-30 15:25:48 +0800

    ## [0.1.1]
    * add deleteAll parameter for SpecialTextSpan

--- lib/src/special_text_span.dart
+++ lib/src/special_text_span.dart
@@ -5,14 +5,25 @@ import 'package:flutter/material.dart';
 ///
 
 class SpecialTextSpan extends TextSpan {
+  /// actual text
   final String actualText;
+
+  /// the start index in all text
   final int start;
+
+  /// the end index in all text
   int get end => start + actualText.length;
+
+  ///extended_text_field
+  ///whether delete all actual text when it try to delete SpecialTextSpan(like a image span)
+  final bool deleteAll;
+
   SpecialTextSpan({
     TextStyle style,
     @required String text,
     @required this.actualText,
     @required this.start,
+    this.deleteAll,
   })  : assert(text != null),
         assert(actualText != null),
         assert(start != null),
