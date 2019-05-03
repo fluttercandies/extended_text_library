29cf084e6e8071e636b1a85a9a5e0b7decd695c0
zmtzawqlp <zmtzawqlp@live.com>
2019-05-03 21:20:26 +0800

    ## [0.1.8]
    * add BackgroundTextSpan to support paint custom background

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
-## [0.1.7]
+## [0.1.8]
 
-* add BackgroundTextSpan to support custom background
+* add BackgroundTextSpan to support paint custom background
 
 ## [0.1.6]
 
--- lib/extended_text_library.dart
+++ lib/extended_text_library.dart
@@ -5,3 +5,5 @@ export 'src/extended_text_utils.dart';
 export 'src/image_span.dart';
 export 'src/special_text_span.dart';
 export 'src/special_text_span_builder.dart';
+export 'src/background_text_span.dart';
+export 'src/text_painter_helper.dart';
